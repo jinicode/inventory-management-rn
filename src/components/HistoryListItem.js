abeb7b10ddd9ec4e0f6b6c4d188a31501cc029c9
jinit <jinit2000@yahoo.com>
2020-07-26 02:11:23 +0530

    download not working

--- src/components/HistoryListItem.js
+++ src/components/HistoryListItem.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
 import {CardItem} from 'native-base';
-import {StyleSheet, ScrollView, View, Text,PermissionsAndroid} from 'react-native';
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import RNFetchBlob from 'rn-fetch-blob'
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  PermissionsAndroid,
+  Platform,
+  AsyncStorage,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import RNFetchBlob from 'rn-fetch-blob';
 
 export default class HistoryListItem extends React.Component {
   componentDidMount() {
@@ -21,40 +29,61 @@ export default class HistoryListItem extends React.Component {
     return transactions.reduce((acc, obj) => acc + obj.rate * obj.quantity, 0);
   };
   //row=this.props.item could have done this rather than writing this.props.item evrywhere
-  actualDownload = (id) => {
-    const { dirs } = RNFetchBlob.fs;
-   RNFetchBlob.config({
-     fileCache: true,
-     addAndroidDownloads: {
-     useDownloadManager: true,
-     mime: 'application/pdf',
-     notification: true,
-     mediaScannable: true,
-     title: 'transactions/'+`${id}`+'.pdf',
-     path: `${dirs.DownloadDir}/transactions${id}.pdf`,
-     },
-   })
-     .fetch('GET', `http://chouhanaryan.pythonanywhere.com/api/pdf/${id}`, {})
-     .then((res) => {
-       console.log('The file saved to ', res.path());
-     })
-     .catch((e) => {
-       console.log(e)
-     });
- }
-
-  downloadFile =async (id) => {
-    try {
-        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE);
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          this.actualDownload(id);
-        } else {
-          Alert.alert('Permission Denied!', 'You need to give storage permission to download the file');
+  actualDownload = async id => {
+    const auth_key = await AsyncStorage.getItem('auth_key');
+    const dirs = RNFetchBlob.fs.dirs;
+    const android = RNFetchBlob.android;
+    RNFetchBlob.config({
+      fileCache: true,
+      addAndroidDownloads: {
+        useDownloadManager: true,
+        mime: 'application/pdf',
+        notification: true,
+        mediaScannable: true,
+        title: 'test.pdf',
+        path: `${dirs.DownloadDir}/test.pdf`,
+      },
+    })
+      .fetch(
+        `http://chouhanaryan.pythonanywhere.com/api/pdf/${id}`,
+        {
+          method: "GET",
+          headers: {
+            "Authorization": `Token ${auth_key}`,
+            "Content-Type": "application/json",
+          }
+        
+        },
+        {'Cache-Control': 'no-store'},
+      )
+      .then(res => {
+        console.log(res);
+        if ((Platform.OS = 'android')) {
+          android.actionViewIntent(res.path(), 'application/pdf');
         }
-      } catch (err) {
-        console.warn(err);
-      } 
-  }
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  downloadFile = async id => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        this.actualDownload(id);
+      } else {
+        Alert.alert(
+          'Permission Denied!',
+          'You need to give storage permission to download the file',
+        );
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
 
   render() {
     return (
@@ -73,7 +102,15 @@ export default class HistoryListItem extends React.Component {
           <Text style={listItemStyles.product}>{this.props.item.name}</Text>
           <Text style={listItemStyles.items}>{this.props.item.quantity}</Text>
           <Text style={listItemStyles.price}>{this.props.item.rate}</Text>
-<Icon name="download" size={30} color="black" style={{flex:0.1}} onPress={()=>{this.downloadFile(this.props.item.id)}}/>
+          <Icon
+            name="download"
+            size={30}
+            color="black"
+            style={{flex: 0.1}}
+            onPress={() => {
+              this.downloadFile(this.props.item.id);
+            }}
+          />
         </CardItem>
       </View>
     );
@@ -94,16 +131,15 @@ const listItemStyles = StyleSheet.create({
   product: {
     flex: 0.3,
     fontSize: 16,
-    marginLeft:-10
+    marginLeft: -10,
   },
   items: {
     flex: 0.25,
     fontSize: 16,
-    marginLeft:5
+    marginLeft: 5,
   },
   price: {
     flex: 0.2,
     fontSize: 16,
-    
   },
 });
